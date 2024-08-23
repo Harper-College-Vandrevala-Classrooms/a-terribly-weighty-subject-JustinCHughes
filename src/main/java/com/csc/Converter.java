13cc8e3413ec4f0a650b65d8dbc08c30a6c59d8d
JustinCHughes <hcjustin713@gmail.com>
2024-08-23 13:12:35 -0500

    Added logic for toPounds and toPoundsAndOunces. Tested logic and passed tests

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -3,10 +3,24 @@ package com.csc;
 public class Converter {
 
   public String toPounds(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPounds'");
+    String ansPhrase = String.format("%.4f", (float) ounces / 16);
+    if (ansPhrase.equals("1.0000")) {
+      return (ansPhrase + " lb");
+    }
+    else {
+      return (ansPhrase + " lbs");
+    }
   }
 
   public String toPoundsAndOunces(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPoundsAndOunces'");
+    int poundsAns = ounces / 16;
+    int ouncesAns = ounces % 16;  
+
+    if (poundsAns == 1) {
+      return (Integer.toString(poundsAns) + " lb " + Integer.toString(ouncesAns) + " oz");
+    }
+    else {
+      return (Integer.toString(poundsAns) + " lbs " + Integer.toString(ouncesAns) + " oz");
+    }
   }
 }
